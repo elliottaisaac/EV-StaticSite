6ba0654d06f694b81db61539c7307db6e05d4741
Elliott Isaac <eisaac22@gmail.com>
2020-04-16 11:24:18 -0500

    EV static first commit

---
+++ JS/scripts.js
@@ -0,0 +1,40 @@
+let isMobile = false;
+if(document.documentElement.clientWidth < 600) isMobile = true;
+
+//nav scripts ------------------------------------------------------------------------------------------->
+let menuOpen = false;
+document.querySelector("#hamburger").addEventListener("click", () => {
+    let menu = document.querySelector("#hamburgerMenu");
+    let nav = document.querySelector("nav");
+    let navMenus = document.querySelector("#masthead");
+    if(menuOpen == false){
+        menu.style.opacity = "1";
+        menu.style.height = "140px";
+        document.querySelector("#hamburger svg:nth-child(3)").style.opacity = "0";
+        document.querySelector("#hamburger svg:nth-child(1)").style.transform = "rotate(45deg) translate(8px, 8px)";
+        document.querySelector("#hamburger svg:nth-child(2)").style.transform = "rotate(-45deg)";
+        document.querySelectorAll("#hamburgerMenu li a").forEach(link =>{
+            link.style.marginLeft = "0px";
+        }); 
+        if(isMobile){
+            document.querySelector("main").style.display = "none";
+            menu.style.border = "none";
+        }
+        menuOpen = true;
+    }
+    else if(menuOpen == true){
+        menu.style.opacity = "0";
+        menu.style.height = "0px";
+        document.querySelector("#hamburger svg:nth-child(3)").style.opacity = "1";
+        document.querySelector("#hamburger svg:nth-child(2)").style.transform = "unset";
+        document.querySelector("#hamburger svg:nth-child(1)").style.transform = "unset";
+        document.querySelector("main").style.display = "block";
+        document.querySelectorAll("#hamburgerMenu li a").forEach(link =>{
+            link.style.marginLeft = "-300px";
+        }); 
+        menuOpen = false;
+    }
+});
+// -------------------------------------------------------------------------------------------> nav scripts
+
+
