4b39f80231f1ec1677dc6a7e7688a12a5de78f50
Zoey Reyes <42983147+TheQuestCrafter@users.noreply.github.com>
2020-04-29 23:11:51 -0500

    Hub Nav Near Complete
    Fully function for the purposes of the site, it just needs plugged in and filled out by the data entry team and some final visual add-ins to bring it up to the proposal's visuals.

--- JS/scripts.js
+++ JS/scripts.js
@@ -45,4 +45,82 @@ document.querySelector("#hamburger").addEventListener("click", () => {
 });
 // -------------------------------------------------------------------------------------------> nav scripts
 
+//hubnav ------------------------------------------------------------------------------------------->
+document.querySelector("#Hub1").addEventListener("mouseover", () => {
+    let textDescription = document.querySelector("#hubDescriptionText");
+    let hubTitle = document.querySelector("#hubTitle");
+    let picture1 = document.querySelector("#mainHubPic");
+    let picture2 = document.querySelector("#gallery1");
+    let picture3 = document.querySelector("#gallery2");
+    let picture4 = document.querySelector("#gallery3")
+    hubTitle.textContent = "Back Of The Yard";
+    textDescription.textContent = "The #LetUsBreathe Collective is an alliance of artists and activists organizing through a creative lens to imagine a world without prisons and police. We organize artists to love and transform themselves, their families, their communities, and their cities through radical imagination and healing. The Collective produces cultural events and direct actions that disrupt oppressive systems, amplify marginalized serve people and communities most directly harmed by mass incarceration, police violence, and systemic injustice.";
+});
+
+document.querySelector("#Hub2").addEventListener("mouseover", () => {
+    let textDescription = document.querySelector("#hubDescriptionText");
+    let hubTitle = document.querySelector("#hubTitle");
+    let picture1 = document.querySelector("#mainHubPic");
+    let picture2 = document.querySelector("#gallery1");
+    let picture3 = document.querySelector("#gallery2");
+    let picture4 = document.querySelector("#gallery3")
+    hubTitle.textContent = "Bright Star Community Outreach";
+    textDescription.textContent = "Bright Star Community Outreach Text here.";
+});
+
+document.querySelector("#Hub3").addEventListener("mouseover", () => {
+    let textDescription = document.querySelector("#hubDescriptionText");
+    let hubTitle = document.querySelector("#hubTitle");
+    let picture1 = document.querySelector("#mainHubPic");
+    let picture2 = document.querySelector("#gallery1");
+    let picture3 = document.querySelector("#gallery2");
+    let picture4 = document.querySelector("#gallery3")
+    hubTitle.textContent = "Open Center For The Arts";
+    textDescription.textContent = "Open Center For The Arts Text here.";
+});
+
+document.querySelector("#Hub4").addEventListener("mouseover", () => {
+    let textDescription = document.querySelector("#hubDescriptionText");
+    let hubTitle = document.querySelector("#hubTitle");
+    let picture1 = document.querySelector("#mainHubPic");
+    let picture2 = document.querySelector("#gallery1");
+    let picture3 = document.querySelector("#gallery2");
+    let picture4 = document.querySelector("#gallery3")
+    hubTitle.textContent = "BBF Family Services";
+    textDescription.textContent = "BBF Family Services Text here.";
+});
+
+document.querySelector("#Hub5").addEventListener("mouseover", () => {
+    let textDescription = document.querySelector("#hubDescriptionText");
+    let hubTitle = document.querySelector("#hubTitle");
+    let picture1 = document.querySelector("#mainHubPic");
+    let picture2 = document.querySelector("#gallery1");
+    let picture3 = document.querySelector("#gallery2");
+    let picture4 = document.querySelector("#gallery3")
+    hubTitle.textContent = "Circles & Ciphers";
+    textDescription.textContent = "Circles & Ciphers Text here.";
+});
+
+document.querySelector("#Hub6").addEventListener("mouseover", () => {
+    let textDescription = document.querySelector("#hubDescriptionText");
+    let hubTitle = document.querySelector("#hubTitle");
+    let picture1 = document.querySelector("#mainHubPic");
+    let picture2 = document.querySelector("#gallery1");
+    let picture3 = document.querySelector("#gallery2");
+    let picture4 = document.querySelector("#gallery3")
+    hubTitle.textContent = "Just Art";
+    textDescription.textContent = "Just Art Text here.";
+});
+
+document.querySelector("#Hub7").addEventListener("mouseover", () => {
+    let textDescription = document.querySelector("#hubDescriptionText");
+    let hubTitle = document.querySelector("#hubTitle");
+    let picture1 = document.querySelector("#mainHubPic");
+    let picture2 = document.querySelector("#gallery1");
+    let picture3 = document.querySelector("#gallery2");
+    let picture4 = document.querySelector("#gallery3")
+    hubTitle.textContent = "Free Write Arts And Literacy Outreach";
+    textDescription.textContent = "Free Write Arts And Literacy Text here.";
+});
+// -------------------------------------------------------------------------------------------> hubnav scripts
 
